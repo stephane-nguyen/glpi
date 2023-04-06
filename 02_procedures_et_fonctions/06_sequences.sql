6578b54931f4cc0b49c1811a4d6a404f44a68c68
vokytruong.pro <vokytruong.pro@gmail.com>
2023-04-06 04:32:55 +0200

    dernière version

--- 02_procedures_et_fonctions/06_sequences.sql
+++ 02_procedures_et_fonctions/06_sequences.sql
@@ -14,20 +14,14 @@ set linesize 250;
 
 
 /***********************************************/
--- Supprimer les résultats, afin de pouvoir recommencer la séquence
-BEGIN
-  FOR i IN 1006..1015 LOOP
-    DELETE FROM user_cergy WHERE id = i;
-  END LOOP;
-END;
-/
-
-
 -- Génération d'utilisateurs au hasard dans la table user_cergy
 DROP SEQUENCE user_cergy_seq;
 CREATE SEQUENCE user_cergy_seq START WITH 1006 INCREMENT BY 1 MAXVALUE 1015 NOCYCLE NOCACHE;
-BEGIN   
+BEGIN
+  FOR i IN 1006..1015 LOOP
+    DELETE FROM computer_device_cergy WHERE id = i;   
     INSERT INTO user_cergy (id, firstname, lastname, email) VALUES (user_cergy_seq.NEXTVAL, 'UTILISATEURS', 'SEQUENCES', 'UTILISATEURS@SEQUENCES.fr');
+  END LOOP;
 END;
 /
 --Vérification
@@ -38,21 +32,14 @@ SELECT * FROM user_cergy;
 
 
 /***********************************************/
--- Supprimer les résultats, afin de pouvoir recommencer la séquence
-BEGIN
-  FOR i IN 3006..3015 LOOP
-    DELETE FROM computer_device_cergy WHERE id = i;
-  END LOOP;
-END;
-/
-
-
 -- Insertion d'outils au hasard dans la table computer_device_cergy
 DROP SEQUENCE seq_tool_cergy;
 CREATE SEQUENCE seq_tool_cergy START WITH 3006 INCREMENT BY 1 MAXVALUE 3015 NOCYCLE NOCACHE;
 DECLARE
   tool_name VARCHAR2(50);
 BEGIN
+  FOR i IN 3006..3015 LOOP
+    DELETE FROM computer_device_cergy WHERE id = i;
     CASE round(dbms_random.value(1, 5))
       WHEN 1 THEN tool_name := 'souris';
       WHEN 2 THEN tool_name := 'clavier';
@@ -61,6 +48,7 @@ BEGIN
       WHEN 5 THEN tool_name := 'imprimante';
     END CASE;    
     INSERT INTO computer_device_cergy (id, name) VALUES (seq_tool_cergy.NEXTVAL, tool_name);
+  END LOOP;
 END;
 /
 --Vérification
